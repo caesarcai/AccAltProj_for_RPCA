56a86d49fbee142b1aaa07b08db747e3f1359c25
caesarcai <44795174+caesarcai@users.noreply.github.com>
2018-11-15 16:07:22 -0800

    Add files via upload

--- test_AccAltProj.m
+++ test_AccAltProj.m
@@ -21,12 +21,12 @@ norm_of_S_true = norm(S_true,'fro');
 
 D = L_true + S_true;
 
-%% RieAltProj without trim,, using all default parameters
+%% AccAltProj without trim,, using all default parameters
 [L1, S1] = AccAltProj( D, r, '' );
 L1_err = norm(L1-L_true,'fro')/norm(L_true,'fro')
 
 
-%% RieAltProj with trim
+%% AccAltProj with trim
 para.mu        = 1.1*get_mu_kappa(L_true,r);  
 para.beta_init = r*sqrt(para.mu(1)*para.mu(end))/(sqrt(m*n));
 para.beta      = r*sqrt(para.mu(1)*para.mu(end))/(4*sqrt(m*n));
